6a673ad63960eaa99c38df663f0520c4357355bd
LapsTimeOFF <quentinloulou31@gmail.com>
2020-12-13 12:58:11 +0100

    :sparkles: Mise à jour

---
+++ src/import/file.js
@@ -0,0 +1,16 @@
+import * as fs from "fs";
+
+
+const log = function(data, file2) {
+  const file = file2 || "./src/logs/preinstall.log"
+  const text = `[LOG] [${Date(Date.now())}] ${data}\n`
+  fs.appendFile(file, text, (err) => { 
+    if (err) 
+      console.log(`Erreur lors de l'écriture de ${file}. Erreur retourné par fs : ${err}`);
+     else { 
+      return 1;
+    } 
+  }); 
+};
+
+export { log };
